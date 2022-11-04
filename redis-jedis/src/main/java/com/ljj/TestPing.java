3977fc8af5eb804af982fe17f02bd708d9fd030b
liujiajie <1852037310@qq.com>
2022-11-04 10:35:27 +0800

    最后一次修改了

--- redis-jedis/src/main/java/com/ljj/TestPing.java
+++ redis-jedis/src/main/java/com/ljj/TestPing.java
@@ -10,5 +10,7 @@ public class TestPing {
         System.out.println("连接redis成功");
         System.out.println("连接redis成功");
         System.out.println("连接redis成功");
+        System.out.println("连接redis成功");
+        System.out.println("连接redis成功");
     }
 }
